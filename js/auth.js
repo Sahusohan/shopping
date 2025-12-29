0b1c7375e777a4a15a1d9476d377743b7ed68fa9
iaminvincible <143888951+Sahusohan@users.noreply.github.com>
2025-12-29 20:49:21 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,17 @@
+function signup(){
+  auth.createUserWithEmailAndPassword(email.value, password.value)
+    .then(()=>location.href="index.html")
+    .catch(e=>alert(e.message));
+}
+
+function login(){
+  auth.signInWithEmailAndPassword(email.value, password.value)
+    .then(()=>location.href="index.html")
+    .catch(e=>alert(e.message));
+}
+
+auth.onAuthStateChanged(user=>{
+  if(!user && location.pathname.includes("admin")){
+    location.href="login.html";
+  }
+});
