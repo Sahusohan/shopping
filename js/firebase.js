c6e75526565df76caffe353f38f0e963a0056ab6
iaminvincible <143888951+Sahusohan@users.noreply.github.com>
2025-12-29 20:48:47 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+ apiKey: "AIzaSyAisU7STb4UAJmcpuFtvp520OrX0of-THI",
+  authDomain: "anonymousconfession-19707.firebaseapp.com",
+  projectId: "anonymousconfession-19707",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
